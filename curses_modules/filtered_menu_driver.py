7174364bc30e58c72cba024d0c080b9547eeb8b9
tacoder <abhinav.singh21093@gmail.com>
2018-10-20 05:23:53 +0530

    Made driver also part of library

---
+++ curses_modules/filtered_menu_driver.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import sys,os
+import curses
+import string
+from filtering_menu  import FilteringMenu
+import curses
+import logging, logging.config
+logger = logging.getLogger(__name__)
+
+def isAlpha(asciiInt):
+    return asciiInt in  [ord(c) for c in string.printable]
+
+def draw_menu(stdscr, menuItems):
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
+
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    m = FilteringMenu(stdscr,menuItems)
+    m.render()
+
+
+    k=0
+    # Loop where k is the last character pressed
+    while (True):
+        logger.debug("Key pressed : " + str(k))
+        logger.debug(k)
+        logger.debug(k == 10    )
+
+        # Initialization
+        # stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        if k == curses.KEY_DOWN:
+            m.down()
+        elif k == curses.KEY_UP:
+            m.up()
+        elif k == curses.KEY_NPAGE:
+            m.pagedown()
+        elif k == curses.KEY_PPAGE:
+            m.pageup()
+        elif k == curses.KEY_BACKSPACE or k ==127:
+            m.removeSearchCh()
+        elif k == curses.KEY_ENTER or k == 10:
+            logger.debug("Retunging.")
+            return m.getCurrentlySelectedItem()
+        elif k == 27: #Escape key, exit with non zero code
+            sys.exit(-1)
+        elif isAlpha(k):
+            m.addSearchCh(str(unichr(k)))
+
+        # Refresh the screen
+        stdscr.refresh()
+
+        # Wait for next input
+        k = stdscr.getch()
+
+def drive(menuItems):
+    os.environ.setdefault('ESCDELAY', '25')
+    return curses.wrapper(draw_menu, menuItems)
--- filtered_menu_example.py
+++ filtered_menu_example.py
@@ -1,85 +1,10 @@
 #!/usr/bin/python
-import sys,os
-import curses
-import json
-import string
-
-from curses_modules.filtering_menu  import FilteringMenu
-import curses
+import sys
+from curses_modules.filtered_menu_driver import drive
 import logging, logging.config
+
 logger = logging.getLogger(__name__)
 
-def isAlpha(asciiInt):
-    return asciiInt in  [ord(c) for c in string.printable]
-
-def draw_menu(stdscr, menuItems):
-    k=0
-    cursor_x = 0
-    cursor_y = 0
-
-    # Clear and refresh the screen for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
-
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    m = FilteringMenu(stdscr,menuItems)
-    m.render()
-
-#    m.increaseOffset()
-
-
-    # Loop where k is the last character pressed
-    while (k != ord('q')):
-        logger.debug("Key pressed : " + str(k))
-        logger.debug(k)
-        logger.debug(k == 10    )
-
-        # Initialization
-        # stdscr.clear()
-        height, width = stdscr.getmaxyx()
-
-        if k == curses.KEY_DOWN:
-            m.down()
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            m.up()
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_NPAGE:
-            m.pagedown()
-        elif k == curses.KEY_PPAGE:
-            m.pageup()
-            cursor_x = cursor_x - 1
-        elif k == curses.KEY_BACKSPACE or k ==127:
-            m.removeSearchCh()
-        elif k == curses.KEY_ENTER or k == 10:
-            logger.debug("Retunging k yooo")
-            return m.getCurrentlySelectedItem()
-        elif k == 27: #Escape key, exit with non zero code
-            sys.exit(-1)
-        elif isAlpha(k):
-            m.addSearchCh(str(unichr(k)))
-
-        logger.debug("curses.KEY_ENTER" + str(curses.KEY_ENTER))
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
-
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
-
-        # Refresh the screen
-        stdscr.refresh()
-
-        # Wait for next input
-        k = stdscr.getch()
-
-def drive(menuItems):
-    os.environ.setdefault('ESCDELAY', '25')
-    return curses.wrapper(draw_menu, menuItems)
 
 def main():
     if "-debug" in sys.argv:
