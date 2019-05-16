5cb98ec99f6ec0c3844dcaeff2bba6b632e30122
tacoder <abhinav.singh21093@gmail.com>
2019-05-16 08:26:41 +0530

    Adding scriptw

--- tree-output-parser/traverse.py
+++ tree-output-parser/traverse.py
@@ -1,7 +1,37 @@
 import json
 from pprint import pprint
+import Queue
 
-with open('HDDJsonTreeWithAggregationSize.json') as f:
-    data = json.load(f)
+mediaFileFormats = ['md','webm','mkv','flv','flv','vob','ogv','ogg','drc','gif','gifv','mng','avi','MTS','M2TS','mov','qt','wmv','yuv','rm','rmvb','asf','amv','mp4','m4p','m4v','mpg','mp2','mpeg','mpe','mpv','mpg','mpeg','m2v','m4v','svi','3gp','3g2','mxf','roq','nsv','flv','f4v','f4p','f4a','f4b']
 
-pprint(data)
+def performActionOnFile(entry):
+	print("Got file to process !!!")
+	print(entry)
+	name = entry['name']
+	extension = name.split('.')[-1]
+	print("Extention is " + extension)
+	if extension in mediaFileFormats:
+		print("This is a medis !!!")
+
+def traverse(data):
+    q = Queue.Queue()
+    q.put(data)
+    while not q.empty():
+        folder = q.get()
+        for entry in folder:
+            # print("Entry!")
+            # print(entry)
+            # print(entry['type'])
+            type = entry['type']
+            if type == 'directory':
+            	q.put(entry['contents'])
+            elif type == 'file':
+            	performActionOnFile(entry)
+
+def processFile(fileName):
+	with open(fileName) as f:
+	    data = json.load(f)
+	    traverse(data)
+
+
+processFile('smallTreeWithAggregations.json')
