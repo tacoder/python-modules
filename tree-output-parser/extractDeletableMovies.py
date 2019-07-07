1e9e9e3144ba1396df46258aaa13250afd505fae
tacoder <abhinav.singh21093@gmail.com>
2019-07-08 03:00:44 +0530

    Adding script to shortlist movies for deletiong

---
+++ tree-output-parser/extractDeletableMovies.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import json
+
+with open("finalResult5.json", 'r') as f:
+    moviesJson = json.load(f)
+
+# moviesJson = json.loads("finalResult5.json")
+
+def notSoGoodMovie(movieData):
+	if movieData['genres'] and  'Comedy' in movieData['genres']:
+		if movieData['rating'] is not None  and movieData['rating'] < 3:
+			return True
+	else:
+		if movieData['rating'] is not None  and movieData['rating'] < 7:
+			return True;
+	return False
+
+for movie in moviesJson:
+	if len(movie['finalResult']) == 0:
+		print "No data for file: " , movie['fullFilePath']
+		continue
+	movieData = movie['finalResult'][0]
+	if notSoGoodMovie(movieData):
+		print "You can delete: " , movie['fullFilePath']
+	else :
+		print "You cannot delete this mf!", movie['fullFilePath']
